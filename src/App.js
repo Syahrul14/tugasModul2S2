06982be7d120342014d898dcd45fc74ecf76678b
Syahrul14 <syahrul.rizal408@gmail.com>
2022-03-28 14:12:07 +0700

    loop arary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Dataku from "./playlist"
+import data from './Data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return data.map((item) => {
+    const { id, album, name, artists } = item;
+    return (
+      <Dataku key={id}
+        image={album.images[0].url}
+        title={name}
+        albums={album.name}
+        artists={artists[0].name}
+      />
+    );
+  });
+
 }
 
 export default App;
---
+++ src/playlist.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dataku = ({ image, title, albums, artists }) => {
+    return (
+        <div className='container'>
+            <img className='playlist-image' src={image} alt='album' />
+            <div className='playlist-content'>
+                <h1 className='playlist-artist'>{artists}</h1>
+                <h4 className='playlist-album'>{albums}</h4>
+                <h3 className='playlist-title'>{title}</h3>
+                <button type="button" className="playlist-action"> Select </button>
+            </div>
+
+        </div>
+    );
+}
+
+export default Dataku;
